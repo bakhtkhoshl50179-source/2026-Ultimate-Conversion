565bd2b03ef1ff8623bfa6cc11930bc8ba764997
bakhtkhoshl50179-source <bakhtkhoshl50179@masseyhigh.school.nz>
2026-05-21 11:57:18 +1200

    Conversion Instructions Checker

---
+++ C_03_Conversion_int_checker.py
@@ -0,0 +1,30 @@
+# Ask user for an integer between 0 and 1000.
+def num_check(question):
+    error = "Please enter a number that is between 1 and 1000000 inclusive\n"
+    while True:
+
+        response = input(question).lower()
+        if response == "xxx":
+            return response
+
+        try:
+            # ask the user for a number
+            response = int(response)
+
+            # check that the number is between 0 and 1000000
+            if 0 <= response <= 1000000:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine Goes Here
+while True:
+     to_convert = num_check("To convert: ")
+     print("You chose", to_convert)
+
+     if to_convert == "xxx":
+         break
